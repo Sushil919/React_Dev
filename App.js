45ce33244b7991b0017e5626a861970553f43676
Sushil <sushilkumar811544@gmail.com>
2024-03-14 18:19:39 +0530

    nested react structure

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
-const heading = React.createElement("h1", {}, "Hello World from React!");
 
-    const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement(
+    "div",
+    { id: "parent"},
+    React.createElement(
+        "div",
+        { id: "child"},
+        React.createElement("h1", {}, "I'm an h1 tag")
+    )
+);
 
-    root.render(heading);
+
+console.log(parent); //object
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
