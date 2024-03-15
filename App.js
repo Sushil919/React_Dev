f98b79dc185d455ebac675340fefd68e771644b8
Sushil <sushilkumar811544@gmail.com>
2024-03-15 17:42:01 +0530

    with all this parcel, we've created our own CRA, production ready code..Ignited our app...Bye Bye Epi-2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 const parent = React.createElement(
     "div",
@@ -5,7 +8,7 @@ const parent = React.createElement(
     [React.createElement(
         "div",
         { id: "child"},[
-        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h1", {}, "This is namaste react🚀"),
         React.createElement("h2", {}, "I'm an h2 tag")
         ]),
         React.createElement(
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#react
+# react🚀
+
+# Parcel
