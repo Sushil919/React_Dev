c10414bb58801258e34d65af59a47ff362d2c70f
Sushil <sushilkumar811544@gmail.com>
2024-03-17 16:10:21 +0530

    comment me likh diya how the code is working

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//React.createElement ==> ReactElement-JS Object ==> HTMLElement(render)
+
 const heading = React.createElement(
     "h1", 
     { id: "heading"},
      "Namaste React🚀"
      );
+
+
+//JSX ==> React.createElement ==> ReactElement-JS Object ==> HTMLElement(render)
 
 const jsxHeading = <h1 id="heading">Namaste React using JSX🚀🚀</h1>
 console.log(jsxHeading);
