0b1c3df0a736bc391e12a63fb793e265e6ae84cc
Sushil <sushilkumar811544@gmail.com>
2024-03-19 15:21:15 +0530

    destructuring on the fly

--- App.js
+++ App.js
@@ -37,8 +37,7 @@ const Header = () => {
     );
 }
 
-const RestaurantCard = (props) => {
-    console.log(props);
+const RestaurantCard = ({resName, cuisine}) => {
     return (
         <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
            <img
@@ -46,10 +45,10 @@ const RestaurantCard = (props) => {
             alt="res-logo" 
             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/c99751d54e4e1847186c62b3309c1327"
            />
-            <h3>{props.resName}</h3>
+            <h3>{resName}</h3>
             <h4>*4</h4>
             <h4>25-30 mins</h4>
-            <p>{props.cuisine}</p>
+            <p>{cuisine}</p>
         </div>
 
     );
