acc105ad2cf34774a5187797dac85e5dc791ac41
Sushil <sushilkumar811544@gmail.com>
2024-03-19 14:39:17 +0530

    made one card

--- App.js
+++ App.js
@@ -39,8 +39,16 @@ const Header = () => {
 
 const RestaurantCard = () => {
     return (
-        <div className="res-card">
-            <h3>Meghna Foods</h3>
+        <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+           <img
+            className="res-logo" 
+            alt="res-logo" 
+            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/c99751d54e4e1847186c62b3309c1327"
+           />
+            <h3>Big Bowl</h3>
+            <h4>*4</h4>
+            <h4>25-30 mins</h4>
+            <p>Chinese, Tibetan, Desserts Moti Jheel</p>
         </div>
     );
 };
