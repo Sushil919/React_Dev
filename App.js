37871c790dfa87081327d865ade498ca4d892735
Sushil <sushilkumar811544@gmail.com>
2024-03-19 15:17:18 +0530

    making dynamic card using PROPS

--- App.js
+++ App.js
@@ -37,7 +37,8 @@ const Header = () => {
     );
 }
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    console.log(props);
     return (
         <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
            <img
@@ -45,10 +46,10 @@ const RestaurantCard = () => {
             alt="res-logo" 
             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/c99751d54e4e1847186c62b3309c1327"
            />
-            <h3>Big Bowl</h3>
+            <h3>{props.resName}</h3>
             <h4>*4</h4>
             <h4>25-30 mins</h4>
-            <p>Chinese, Tibetan, Desserts Moti Jheel</p>
+            <p>{props.cuisine}</p>
         </div>
 
     );
@@ -61,8 +62,14 @@ const Body = () => {
                 Search
             </div>
             <div className="res-container">
-                <RestaurantCard />
-                <RestaurantCard />
+                <RestaurantCard 
+                resName="Big Bowl"
+                cuisine="Chinese, Tibetan, Desserts Moti Jheel"
+                />
+                <RestaurantCard 
+                resName="Food Carnival"
+                cuisine="Biryani, South Indian"
+                />
                 <RestaurantCard />
                 <RestaurantCard />
                 <RestaurantCard />
