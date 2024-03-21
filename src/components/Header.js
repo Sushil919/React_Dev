dd5b5a54615811959f2596832fe261209b3e88c2
Sushil <sushilkumar811544@gmail.com>
2024-03-21 18:11:12 +0530

    Header is now new component

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img src="https://thumbs.dreamstime.com/z/print-food-logo-food-delivery-logo-vector-180672311.jpg" className="logo" />
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
