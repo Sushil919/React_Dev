89935f34ec0e97868db8435122b779352f32534e
Sushil <sushilkumar811544@gmail.com>
2024-03-21 15:41:31 +0530

    thoda code clean kiya, destructure kar ke

--- App.js
+++ App.js
@@ -40,6 +40,14 @@ const Header = () => {
 const RestaurantCard = (props) => {
     const {resData} = props;
 
+    const {
+        cloudinaryImageId,
+        name,
+        cuisines,
+        avgRatingString,
+        costForTwo,
+    } = resData?.info;
+
     return (
         <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
            <img
@@ -48,10 +56,10 @@ const RestaurantCard = (props) => {
             src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resData.info.cloudinaryImageId}
             />
            
-            <h3>{resData.info.name}</h3>
-            <h3>{resData.info.cuisines.join(", ")}</h3>
-            <h4>{resData.info.avgRatingString}</h4>
-            <h4>{resData.info.costForTwo}</h4>
+            <h3>{name}</h3>
+            <h3>{cuisines.join(", ")}</h3>
+            <h4>{avgRatingString}</h4>
+            <h4>{costForTwo}</h4>
             <h4>{resData.info.sla.deliveryTime} Minutes</h4>
         </div>
 
