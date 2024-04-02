4b044611b1807985a0901cbb323f1b85d05b376d
Sushil <sushilkumar811544@gmail.com>
2024-04-02 13:46:11 +0530

    trying to update the data storing inside a normal variable -->conole me to aa rha filter hoke... but UI not getting updated...need to use HOOK =>StateVariable -> useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,51 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
 
 const Body = () => {
+    let listOfRestaurants = [
+        {
+            "info": {
+            "id": "126289",
+            "name": "UBQ by Barbeque Nation",
+            "cloudinaryImageId": "laiyciem5yqr1qbxhfcj",
+            "costForTwo": "₹300 for two",
+            "cuisines": ["North Indian","Barbecue","Biryani","Kebabs","Mughlai","Desserts"],
+            "avgRatingString": "4.3",
+            "sla": {
+                "deliveryTime": 31,}
+                }
+        },
+        {
+            "info" : {
+            "id": "305577",
+            "name": "Pizza Hut",
+            "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+            "costForTwo": "₹350 for two",
+            "cuisines": ["Pizzas"],
+            "avgRatingString": "3.9",
+            "sla": {
+                "deliveryTime": 31,}
+                }
+        }
+    ];
+
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn"
                 onClick={() => {
-                    console.log("Button Clicked");
+                    //Filter Logic
+                    listOfRestaurants = listOfRestaurants.filter(
+                        (x)=>x.info.avgRatingString>4
+                    );
+                    console.log(listOfRestaurants);
                 }}
                 >
                     Top Rated Restaurants
                 </button>
             </div>
             <div className="res-container">
-                {resList.map((restaurant) => (
-                    <RestaurantCard key = {restaurant.info.id} resData = {restaurant} />
+                {listOfRestaurants.map((restaurant) => (
+                    <RestaurantCard key = {restaurant.info.id} resData={restaurant} />
                 ))}
             </div>
         </div>
