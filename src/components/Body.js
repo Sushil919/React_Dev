9006bfb0acdcd1aa3c25d826f7ea2c887306fff7
Sushil <sushilkumar811544@gmail.com>
2024-04-10 17:48:21 +0530

    Reslove the CORS for now !!

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,7 +12,7 @@ const Body = () => {
 
     const fetchData = async () => {
         const data = await fetch(
-            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.7605545&lng=83.3731675&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+            "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.7605545&lng=83.3731675&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
         );
         const json = await data.json();
 
