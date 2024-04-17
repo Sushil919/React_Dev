f38ab617465db32096651b35c9eb70fd6f4950aa
Sushil <sushilkumar811544@gmail.com>
2024-04-17 22:52:09 +0530

    could not load update cart url

--- src/components/Body.js
+++ src/components/Body.js
@@ -24,19 +24,6 @@ const Body = () => {
         setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
 
-    // useEffect(() => {
-    //     // POST request using fetch inside useEffect React hook
-    //     const requestOptions = {
-    //         method: 'POST',
-    //         headers: { 'Content-Type': 'application/json' },
-    //         body: JSON.stringify({ title: 'React Hooks POST Request Example' })
-    //     };
-    //     fetch('https://www.swiggy.com/dapi/restaurants/list/update', requestOptions)
-    //         .then(response => response.json())
-    //         .then(data => setPostId(data.id));
-    
-    // // empty dependency array means this effect will only run once (like componentDidMount in classes)
-    // }, []);
 
     return listOfRestaurants.length === 0 ? (<Shimmer /> ) : 
     (
