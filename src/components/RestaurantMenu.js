ef238ac27247e2a4def36b194345b3dfd33c7497
Sushil <sushilkumar811544@gmail.com>
2024-05-09 17:56:19 +0530

    Created Restaurant Menu page

---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,17 @@
+const RestaurantMenu = () => {
+    return (
+        <div className="menu"> 
+            <h1>Name of the Restaurant</h1>
+            <h2>Menu</h2>
+
+            <li>
+                <ul>Cuisine</ul>
+                <ul>Quantitiy</ul>
+                <ul>Delivery time</ul>
+                <ul>Cost</ul>
+            </li>
+        </div>
+    )
+}
+
+export default RestaurantMenu;
