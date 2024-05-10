38c9dd2395ba23bb9a24528c4b96e9e763848350
Sushil <sushilkumar811544@gmail.com>
2024-05-10 14:28:38 +0530

    finally, name of Res has come dynamically to us in menu route

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+
 const RestaurantMenu = () => {
-    return (
+    const [resInfo, setResInfo] = useState(null);
+
+    useEffect(() => {
+        fetchMenu();
+    }, []); 
+    //Second one is optional arguement, when we keep it empty and doesn't pass anything, then it will be calles again and again the page renders....=>but we want to render it only once -->> so we'll pass an empty array dependency []
+
+    const fetchMenu = async() => {
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.7605545&lng=83.3731675&restaurantId=126289&catalog_qa=undefined&isMenuUx4=true&submitAction=ENTER");
+
+        const json = await data.json();
+        console.log(json);
+        setResInfo(json.data);
+    };
+
+    return resInfo === null ? (
+        <Shimmer />
+    ) : (
         <div className="menu"> 
-            <h1>Name of the Restaurant</h1>
+            <h1>{resInfo?.cards[2]?.card?.card?.info?.name}</h1>
             <h2>Menu</h2>
 
             <li>
@@ -11,7 +31,7 @@ const RestaurantMenu = () => {
                 <ul>Cost</ul>
             </li>
         </div>
-    )
-}
+    );
+};
 
 export default RestaurantMenu;
