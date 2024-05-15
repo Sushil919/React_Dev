a1beb64580661cc94c23e7d10a505e69dc04acf0
Sushil <sushilkumar811544@gmail.com>
2024-05-15 13:35:41 +0530

    Menu items

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -21,11 +21,23 @@ const RestaurantMenu = () => {
         return <Shimmer />
 
     const { name, cuisines, costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info;
+
+    const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+    console.log(itemCards);
+    
     return (
         <div className="menu"> 
             <h1>{name}</h1>
             <p>{cuisines.join(", ")}-{costForTwoMessage}</p>
-            
+            <h2>Menu</h2>
+            <ul>
+                {itemCards.map((item) => (
+                <li>
+                    {item.card.info.name}
+                </li>
+                ))}
+            </ul>
 
         </div>
     );
